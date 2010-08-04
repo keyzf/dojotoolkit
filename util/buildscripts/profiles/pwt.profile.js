334262cedd600180651cb0b2f29e2268836c1ee0
Rakesh Pai <rakesh.pa@directi.com>
2010-08-04 18:57:37 +0530

    Added the pwt build profile

---
+++ util/buildscripts/profiles/pwt.profile.js
@@ -0,0 +1,15 @@
+dependencies = {
+	layers: [
+		{
+			name: "../pwt/widgets/ContainerPage.js",
+			dependencies: [
+				"pwt.widgets.ContainerPage"
+			]
+		}
+	],
+	prefixes: [
+		["dijit", "../dijit"],
+		["dojox", "../dojox"],
+		["pwt", "../../pwt"]
+	]
+}
